c6db9ecad84100c54f514daa6c5d35d1032c0535
Mr. Rodriguez <123821069+mr-rodriguez@users.noreply.github.com>
2023-02-10 16:23:47 -0600

    Create d3_lovecalculator.py

---
+++ python/d3_lovecalculator.py
@@ -0,0 +1,40 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+# concatenate both names to add number of characters later on
+combined_names = name1 + name2
+
+# the lower() will help when a name is entered with capital letters
+lower_case_string = combined_names.lower()
+
+# count the number of occurences of the given letters in the combined_names
+t = lower_case_string.count("t")
+r = lower_case_string.count("r")
+u = lower_case_string.count("u")
+e = lower_case_string.count("e")
+
+# add the variables for calculating love_score later on
+true = t + r + u + e
+
+l = lower_case_string.count("l")
+o = lower_case_string.count("o")
+v = lower_case_string.count("v")
+e = lower_case_string.count("e")
+
+love = l + o + v + e
+
+# This variable concatenates 'true' and 'love', adds them together as a string, then converts the result into the final number. 
+# If you do not convert to an integer, you get an error
+love_score = int(str(true) + str(love))
+
+if (love_score < 10) or (love_score > 90):
+    print(f"Your score is {love_score}, you go together like coke and mentos.")
+elif (love_score >= 40) and (love_score <= 50):
+    print(f"Your score is {love_score}, you are alright together.")
+else:
+    print(f"Your score is {love_score}.")
